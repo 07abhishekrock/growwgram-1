832da53667bc5ec66792c1101d3874a2a886ba43
anishkoulgi <anish.koulgi@gmail.com>
2022-01-23 22:03:17 +0530

    Basic setup completed

---
+++ src/core/Wrapper.tsx
@@ -0,0 +1,21 @@
+import "styles/paper.css";
+
+import React from "react";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import routes from "../routes/routes";
+
+function Wrapper() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Wrapper;
